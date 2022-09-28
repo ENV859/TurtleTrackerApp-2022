686b271a801fa601df6af4169cfce71a2cdb251b
John Fay <john.fay@duke.edu>
2022-09-28 09:24:59 -0400

    Revert "Switch to while loop"
    This reverts commit 8ef0162123d42399ab44cde5abe9cbbec4a4abfb.

--- ARGOSTrackingTool.py
+++ ARGOSTrackingTool.py
@@ -16,15 +16,16 @@ file_name = 'data/raw/Sara.txt'
 file_object = open(file=file_name,mode='r')
 
 #Read contents of file into a list
-lineString = file_object.readline()
+line_list = file_object.readlines()
+
+#Close the file object
+file_object.close()
 
 #Extract one data line into a variable
-while lineString:
+for lineString in line_list:
 
     #Check to see if the lineString is a data line
-    if lineString[0] in ('#','u'):
-        lineString = file_object.readline()
-        continue
+    if not lineString[0] in ('#','u'):
         continue
     
     #Split lineString into a list of items
@@ -39,9 +40,3 @@ while lineString:
     
     #Print information to the user
     print(f'Record {record_id} indicates Sara was seet and {obs_lat}N, {obs_lon}W  on {obs_date}. ')
-    
-    #Move to the next line in the the file
-    lineString = file_object.readline()
-
-#Close the file object
-file_object.close()
